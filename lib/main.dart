f32b466231660f845e5a4a5c6a7217d63bf100c6
Iván i5 <sayavedrahector@gmail.com>
2022-07-04 21:34:09 -0300

    Commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+      title: "Calculadora IMC",
+      home: Inicio(),
+    );
+  }
+}
+
+class Inicio extends StatelessWidget {
+  const Inicio({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
